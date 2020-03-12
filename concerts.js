400d68f5db47f58f2e82205e7b15e0d095e1a5f0
Odysseus221 <33930108+Odysseus221@users.noreply.github.com>
2020-03-12 17:39:10 +0700

    Created new project and added route for concert

---
+++ concerts.js
@@ -0,0 +1,25 @@
+
+    const concerts = [
+        { id: 1,
+            name: "Bon Jovi", 
+            place: "London"
+        },   
+        { id: 2,
+            name: "Jimmy Page", 
+            place: "NYC"
+        },    
+        { id: 3,
+            name: "Classical", 
+            place: "Rome"
+        },    
+        { id: 4,
+            name: "Middle Eastern", 
+            place: "ATHENS"
+        },   
+         { id: 5,
+            name: "Dekkbe Dancing", 
+            place: "Tel Aviv"
+        }   
+    ]
+
+    module.exports = concerts;
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+const concerts = require('./concerts')
+//console.log(concerts);
+  app.get('/concerts',(req,res)=>{
+    console.log('concerts/')
+    res.json(concerts  )
+
+});   
+  app.get('/concerts/:place', (req,res)=>{
+     console.log(req.params.place)
+//     res.json(concerts[req.params.id])
+
+ //    let concert = concerts.filter(concert =>concert.id== req.params.id)
+     let concert = concerts.filter(concert =>concert.place.toLowerCase()== req.params.place.toLowerCase())
+     //define the variable
+     if (concert && concert.length) {
+         res.json(concert)
+     }
+    else{
+        res.send("there is no concert with this ID")
+    }
+  });
+
+ 
+app.get('/', (req, res)=>{
+    console.log('route/')
+    res.send('Hello from second Try File!')
+});
+app.get('/concerts_tonight', (req, res) =>{
+    console.log('route/concerts_tonight!')
+    res.send('Enclosed is a list of <h1>Happenings Around  Town Tonight</h1>')
+});
+
+
+app.listen(port, () =>{
+    console.log('server has started on port: ' + port);
+
+})
+
+ 
+
+ 
